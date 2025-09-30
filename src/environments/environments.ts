0358e0d5b5d5623390636c7e7a0ef35f43ce271b
Awa425 <diopawa192@gmail.com>
2025-09-30 21:03:47 +0000

    push prod

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,8 @@ export const envVars  = {
 
   apiBaseUrl: 'https://academy.bideewtech.com/api_elearning/index.php/api',
 
-  fileBaseUrl: 'https://academy.bideewtech.com/api_elearning/index.php/api/storage',
+  // fileBaseUrl: 'https://academy.bideewtech.com/api_elearning/index.php/api/storage',
+  fileBaseUrl: 'https://academy.bideewtech.com/api_elearning/storage',
 
   appName: 'Bideew Academy',
   version: '1.0.0-dev',
